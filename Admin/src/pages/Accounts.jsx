9e74867e340dbda44f507d797b446237080a315d
Mehraj16 <mehrajhossain167@gmail.com>
2024-04-03 17:04:52 +0600

    admin updated

--- Admin/src/pages/Accounts.jsx
+++ Admin/src/pages/Accounts.jsx
@@ -1,9 +1,12 @@
-import React, { useState, useEffect } from 'react'
-import AdminSidebar from '../components/AdminSidebar'
-import AdminHeader from '../components/AdminHeader'
+import React, { useState, useEffect } from 'react';
+import AdminSidebar from '../components/AdminSidebar';
+import AdminHeader from '../components/AdminHeader';
 import manage from "../css/manage.module.css";
+
 export default function Accounts() {
   const [profileData, setProfileData] = useState([]);
+  const [showForm, setShowForm] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -17,27 +20,44 @@ export default function Accounts() {
         console.error('Error fetching data:', error);
       }
     };
-  
+
     fetchData();
   }, []);
+
+  const toggleFormVisibility = () => {
+    setShowForm(!showForm);
+  };
 
   return (
     <div className='App'>
       <AdminSidebar />
       <AdminHeader profilepic={`/src/assets/${profileData.profileImage}`}/>
       <div className='Content'>
-      <div>
+        <div>
           <h3 className={manage.headline}>Admin Accounts</h3>
-      </div>
+        </div>
         <div className={manage.jobsCreate}>
           <div>
-            <p>Create New Admin</p>
+            <button onClick={toggleFormVisibility}>Create New Admin</button>
           </div>
           <div>
             <p>View All Admin</p>
           </div>
-      </div>
+        </div>
+        {showForm && (
+          <form className={manage.formCreate}>
+            <div>
+              <label>Email:</label><br />
+              <input type="email" />
+            </div>
+            <div>
+              <label>Password:</label><br />
+              <input type="password" />
+            </div>
+            <button type="submit">Create Account</button>
+          </form>
+        )}
       </div>
     </div>
-  )
+  );
 }
